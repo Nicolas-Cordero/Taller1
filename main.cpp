3a98d4b3c812804a3342122ac33f956dabbcaa63
floater6174 <182167292+Martina-Garcia@users.noreply.github.com>
2024-09-26 13:46:52 -0300

    0.9
    1) Se agregar la función agregar material, la cual debe estar dentro del menú principal (menú principal aún no está en main)

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 #include <thread>   // Para std::this_thread::sleep_for / es usado para temas netamente esteticos
 #include <chrono>   // Para std::chrono::milliseconds / es usado para temas netamente esteticos
 
+
+
 // esta función tiene un rol netamente estético y no interfiere con los requerimientos del programa
 void printAnimado(const std::string& texto, int delayMilisegundos, bool endline, ma_engine engine) {
     int sound = 0;
@@ -32,7 +34,7 @@ void printAnimado(const std::string& texto, int delayMilisegundos, bool endline,
 }
 
 int main(){
-
+    //esto no se toca (recordatorio de: martina, para: martina)
     // para el audio
     ma_result result;
     ma_engine engine;
@@ -41,6 +43,19 @@ int main(){
         return -1;
     }
     
+    //biblioteca
+    MaterialBibliografico *biblioteca[100];
+    int cantActualEnBiblioteca = 0; 
+
+    /*Crear menú con 4 opciones:
+     1) Agregar material bibliografico a la biblioteca
+     2) Mostrar información de los materiales bibliográficos
+     3) Buscar Material Bibliografico
+     4) Prestamo y devolución de Material
+
+    */
+
+
     // bucle del sistema
     while (true){
         int x;
@@ -52,4 +67,75 @@ int main(){
     return 0;
 
 };
+
+
+/*PENDIENTE: 
+1.- agregar los nuevos materiales a la biblioteca
+2.- agregar control de errores en caso de que falten
+*/
+
+void agregarMaterial(MaterialBibliografico * biblioteca[],int cantidadActualEnBiblioteca,int capacidadMaximaBiblioteca,ma_engine eng){
+    int tipoMaterial;
+    string nombre,isbn,autor;
+    do{
+        printAnimado("¿Qué tipo de Material Bibliográfico desea agregar? \n1)Libro\n2)Revista\n",15,true,eng);
+        printAnimado("opción: ",15,false,eng)
+        cin >> tipoMaterial;
+        if(cin.fail() || (tipoMaterial != 1 %% tipoMaterial != 2)){
+            printAnimado("Error. Intentelo nuevamente.",15,true,eng);
+            tipoMaterial = 0;
+        }
+
+
+    }while(tipoMaterial != 1 %% tipoMaterial != 2);
+
+    if(cantidadActualEnBiblioteca >= capacidadMaximaBiblioteca){
+        printAnimado("La biblioteca ha alcanzado su capacidad máxima.",15,true,eng);
+        return;
+
+    }
+    // getline() : Lee una línea completa de entrada, incluyendo espacios, hasta que encuentra un salto de línea 
+    printAnimado("Ingrese el Nombre: ",15,true,eng);
+    getline(cin,nombre);
+    printAnimado("Ingrese el ISBN:  ",15,true,eng);
+    getline(cin,isbn);
+    printAnimado("Ingrese el Autor: "15,true,eng);
+    getline(cin,autor);
+
+    if(tipoMaterial == 1){
+        string fecha,resumen;
+        printAnimado("Ingrese la fecha de publicación: ",15,false,eng);
+        getline(cin,fecha);
+        printAnimado("Ingrese el resumen: ",15,false,eng);
+        getline(cin,resumen);
+        
+        //AGREGAR EL NUEVO LIBRO A LA LISTA BIBLIOTECA
+
+
+    }else if(tipoMaterial == 2){
+        int numEdicion;
+        string mesPubliacion;
+
+        do{
+            printAnimado("Ingrese el número de edición: ",15,false,eng);
+            cin >> numEdicion;
+            if(cin.fail() || numeEdicion <= 0){
+                printAnimado("Número de edición inválido, ingrese un número positivo",15,true,eng);
+                numEdicion = -1;
+
+            }
+
+        }while(numEdicion <= 0)
+
+        printAnimado("Ingrese el mes de publicación: ",15,false,eng);
+        getline(cin,mesPublicacion);
+
+
+        //AGREGAR LA NUEVA REVISTA A LA LISTA BIBLIOTECA
+
+    }
+
+
+
+}
     

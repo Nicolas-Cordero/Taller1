e530fa1ac62e55040b443ef1de0af0d7c66147d7
nicolás <nico.cordero.varas@gmail.com>
2024-10-13 16:52:13 -0300

    1.2
    Se corrige error de compilación y se procederá en siguiente versión a re-escribir la función agregarMaterial que era la causante de el error de compilación.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ void printAnimado(const std::string& texto, int delayMilisegundos, bool endline)
     int tamaño = texto.length();
     for (int i = 0 ; i < tamaño ; i++) {
      
-        std::cout << texto[i] << std::flush;  // Imprime el carácter y vacía el buffer
+        cout << texto[i] << std::flush;  // Imprime el carácter y vacía el buffer
 
         
         std::this_thread::sleep_for(std::chrono::milliseconds(delayMilisegundos));  // Espera el tiempo especificado
@@ -27,8 +27,6 @@ void printAnimado(const std::string& texto, int delayMilisegundos, bool endline)
     
 }
 
-// Forward declaration of printAnimado
-void printAnimado(const std::string& texto, int delayMilisegundos, bool endline);
 
 int main(){
     //esto no se toca (recordatorio de: martina, para: martina)
@@ -59,76 +57,5 @@ int main(){
 
 };
 
-void agregarMaterial(MaterialBibliografico * biblioteca[],int cantidadActualEnBiblioteca,int capacidadMaximaBiblioteca){
-    int tipoMaterial;
-    string nombre,isbn,autor;
-    do{
-        printAnimado("¿Qué tipo de Material Bibliográfico desea agregar? \n1)Libro\n2)Revista\n",15,true);
-        printAnimado("opción: ",15,false);
-        cin >> tipoMaterial;
-        if(cin.fail() || (tipoMaterial != 1 && tipoMaterial != 2)){
-            printAnimado("Error. Intentelo nuevamente.",15,true);
-            tipoMaterial = 0;
-        }
 
-
-    }while(tipoMaterial != 1 && tipoMaterial != 2);
-
-    if(cantidadActualEnBiblioteca >= capacidadMaximaBiblioteca){
-        printAnimado("La biblioteca ha alcanzado su capacidad máxima.",15,true);
-        return;
-
-    }
-    // getline() : Lee una línea completa de entrada, incluyendo espacios, hasta que encuentra un salto de línea 
-    printAnimado("Ingrese el Nombre: ",15,true);
-    getline(cin,nombre);
-    printAnimado("Ingrese el ISBN:  ",15,true);
-    getline(cin,isbn);
-    printAnimado("Ingrese el Autor: ",15,true);
-    getline(cin,autor);
-
-    if(tipoMaterial == 1){
-        bool prestado =true;
-        string fecha,resumen;
-        printAnimado("Ingrese la fecha de publicación: ",15,false);
-        getline(cin,fecha);
-        printAnimado("Ingrese el resumen: ",15,false);
-        getline(cin,resumen);
-        
-        //AGREGAR EL NUEVO LIBRO A LA LISTA BIBLIOTECA
-        MaterialBibliografico *nuevoMaterial = new Libro(nombre,isbn,autor,prestado,fecha,resumen);
-        biblioteca[cantidadActualEnBiblioteca] = nuevoMaterial;
-        cantidadActualEnBiblioteca++;
-
-
-    }else if(tipoMaterial == 2){
-        bool prestado = true;
-        int numEdicion;
-        string mesPublicacion;
-
-        do{
-            printAnimado("Ingrese el número de edición: ",15,false);
-            cin >> numEdicion;
-            if(cin.fail() || numEdicion <= 0){
-                printAnimado("Número de edición inválido, ingrese un número positivo",15,true);
-                numEdicion = -1;
-
-            }
-
-        }while(numEdicion <= 0);
-
-        printAnimado("Ingrese el mes de publicación: ",15,false);
-        getline(cin,mesPublicacion);
-
-
-        //AGREGAR LA NUEVA REVISTA A LA LISTA BIBLIOTECA
-        MaterialBibliografico *nuevoMaterial = new revista(nombre,isbn,autor,prestado,numEdicion,mesPublicacion);
-        biblioteca[cantidadActualEnBiblioteca] = nuevoMaterial;
-        cantidadActualEnBiblioteca++;
-
-    }
-
-
-
-}
     
